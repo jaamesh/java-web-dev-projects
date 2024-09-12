4e0df1f13f3ebca92a2280c4edbf716c50817d05
jaamesh <james.humphrey@gmail.com>
2024-09-12 18:16:43 -0500

    Instantiated Jim

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,12 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student jim = new Student();
+        jim.setName("Jim Humphrey");
+        jim.setStudentId(235456);
+        jim.setGpa(4.62);
+        jim.setNumberOfCredits(32);
+
+        System.out.println("Jim: " + jim.getName());
     }
 }
