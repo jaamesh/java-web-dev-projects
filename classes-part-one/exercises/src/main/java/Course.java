0e115fb28a5d8b0218b1602fc82bcc42daf90be7
jaamesh <james.humphrey@gmail.com>
2024-09-12 18:23:13 -0500

    Added Course and Teacher classes.

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,29 @@
+public class Course {
+    private String courseName;
+    private String courseDescription;
+    private ArrayList<Student> students;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+}
---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,39 @@
+public class Teacher {
+
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private double yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public double getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+    public void setYearsTeaching(double yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+}
