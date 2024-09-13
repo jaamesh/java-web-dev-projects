783086277c75839ff822ada8f51bf0c4754be4e9
jaamesh <james.humphrey@gmail.com>
2024-09-12 19:15:09 -0500

    Defined Menu and MenuItems - props, constructor, getters, setters

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,4 +1,51 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+
+    private String restaurantName;
+    private String gratiuties;
+    private Date lastUpdated;
+    private ArrayList<MenuItem> menuItems;
+
+    public Menu(String restaurantName, String gratiuties, Date lastUpdated, ArrayList<MenuItem> menuItems) {
+        this.restaurantName = restaurantName;
+        this.gratiuties = gratiuties;
+        this.lastUpdated = lastUpdated;
+        this.menuItems = menuItems;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getGratiuties() {
+        return gratiuties;
+    }
+
+    public void setGratiuties(String gratiuties) {
+        this.gratiuties = gratiuties;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,4 +1,48 @@
 package org.launchcode;
 
 public class MenuItem {
+    private double price;
+    private String description;
+    private String category;
+    private Boolean isNew;
+    private static String[] CATEGORY_TYPES = {"appetizer", "main course", "dessert"};
+
+    public MenuItem(double price, String description, String category, Boolean isNew) {
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
 }
